86725ab625fd18d323ff407fa9135f1f6c0f2a89
c.dehondt <c.dehondt@live.com>
2017-07-12 11:46:28 +0200

    Random Game Card Function Programming is ok

--- js/main.js
+++ js/main.js
@@ -1,15 +1,41 @@
 // variable
-var choix1 =document.getElementsByClassName("carte");
-var choix2 =document.getElementsByClassName("carte2");
+var choixJoueur =document.getElementsByClassName("carte");
+var carte= ["img/image1.jpg","img/image2.png","img/image3.jpg","img/image4.jpg",
+            "img/image5.jpeg","img/image6.png","img/image7.jpeg","img/image8.jpg",
+            "img/image1.jpg","img/image2.png","img/image3.jpg","img/image4.jpg",
+            "img/image5.jpeg","img/image6.png","img/image7.jpeg","img/image8.jpg"];
 
-for (var i = 0; i < choix1.length; i++) {
-  choix1[i].onclick = function() {
-  this.src ="img/bravo.png";
-  }
+// var melanger= carte[Math.floor(Math.random()*carte.length)];
+var comparer=[];
+
+
+
+// function
+function shuffleArray(carte) {
+    for (var i = carte.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = carte[i];
+        carte[i] = carte[j];
+        carte[j] = temp;
+    }
+    return carte;
 }
 
-for (var i = 0; i < choix2.length; i++) {
-  choix2[i].onclick = function() {
-  this.src ="img/carte.gif";
-  }
+// script
+shuffleArray(carte);
+for (var i = 0; i < choixJoueur.length; i++) {
+  document.getElementsByTagName("img")[i].src=carte[i];
+  choixJoueur[i].onclick = function () {
+  this.style.opacity= 1;
 }
+}
+
+
+
+
+
+// for (var i = 0; i < choixJoueur2.length; i++) {
+//   choixJoueur2[i].onclick = function() {
+//   this.src ="";
+//   }
+// }
