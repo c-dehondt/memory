ace290faad8e47be673906d570a85e28eb6df9b9
c.dehondt <c.dehondt@live.com>
2017-07-12 16:18:52 +0200

    main.js: Comparaison ajoutée etape 1Addition comparison step 1

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ var carte= ["img/image1.jpg","img/image2.png","img/image3.jpg","img/image4.jpg",
             "img/image1.jpg","img/image2.png","img/image3.jpg","img/image4.jpg",
             "img/image5.jpeg","img/image6.png","img/image7.jpeg","img/image8.jpg"];
 
-// var melanger= carte[Math.floor(Math.random()*carte.length)];
 var comparer=[];
 
 
@@ -21,21 +20,29 @@ function shuffleArray(carte) {
     return carte;
 }
 
+
+function comparaison(){
+
+    for (var i = 0; i < ordi.length; i++){
+      if (ordi[0] == ordi[1]) {
+        alert("bravo");
+      }else {
+        alert("null");
+      }
+    }
+  }
+
+
+
 // script
 shuffleArray(carte);
 for (var i = 0; i < choixJoueur.length; i++) {
   document.getElementsByTagName("img")[i].src=carte[i];
-  choixJoueur[i].onclick = function () {
-  this.style.opacity= 1;
+    choixJoueur[i].onclick = function () {
+      this.style.opacity= 1;
+      comparer.push(this);
+      var ordi=comparer.length == 2;
+      comparaison();
+    }
+
 }
-}
-
-
-
-
-
-// for (var i = 0; i < choixJoueur2.length; i++) {
-//   choixJoueur2[i].onclick = function() {
-//   this.src ="";
-//   }
-// }
