a5a16f95dccecd6c08319a92f3aac6415788b16a
c.dehondt <c.dehondt@live.com>
2017-07-18 11:15:34 +0200

    finish project memory

--- js/main.js
+++ js/main.js
@@ -1,15 +1,40 @@
+
 // variable
 var choixJoueur =document.getElementsByClassName("carte");
-var carte= ["img/image1.jpg","img/image2.png","img/image3.jpg","img/image4.jpg",
-            "img/image5.jpeg","img/image6.png","img/image7.jpeg","img/image8.jpg",
-            "img/image1.jpg","img/image2.png","img/image3.jpg","img/image4.jpg",
-            "img/image5.jpeg","img/image6.png","img/image7.jpeg","img/image8.jpg"];
+var carte= ["img/image1.png","img/image2.png","img/image3.png","img/image4.png",
+            "img/image5.png","img/image6.png","img/image1.png","img/image2.png",
+            "img/image3.png","img/image4.png","img/image5.png","img/image6.png"];
 
 var comparer=[];
-
-
+var nbEssai=10;
+var clickStop=0;
 
 // function
+
+function jouer() {
+  var joueur=prompt("quel est ton pseudo??");
+  document.getElementById("pseudo").innerHTML="Bonjour " + joueur;
+  shuffleArray(carte);
+  for (var i = 0; i < choixJoueur.length; i++) {
+    document.getElementsByTagName("img")[i].src=carte[i];
+    choixJoueur[i].onclick = function () {
+      if (clickStop == 0) {
+        comparer.push(this);
+        this.style.opacity= 1;
+        this.parentElement.classList.remove("dos");
+
+            if(comparer.length == 2){
+                clickStop++;
+                comparaison();
+
+            }
+          }
+        }
+      }
+
+
+}
+
 function shuffleArray(carte) {
     for (var i = carte.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
@@ -22,27 +47,35 @@ function shuffleArray(carte) {
 
 
 function comparaison(){
-
-    for (var i = 0; i < ordi.length; i++){
-      if (ordi[0] == ordi[1]) {
-        alert("bravo");
+      if (comparer[0].src == comparer[1].src) {
+        clickStop=0;
+        comparer=[];
       }else {
-        alert("null");
-      }
-    }
+        nbEssai--;
+        console.log(nbEssai);
+        setTimeout(function () {
+          comparer[0].style.opacity=0;
+          comparer[0].parentElement.classList.add("dos");
+          comparer[1].style.opacity=0;
+          comparer[1].parentElement.classList.add("dos");
+
+          clickStop =0;
+          comparer=[];
+        },600 );
+      }chance();
+
   }
 
+function reset() {
+  nbEssai =10;
+  window.location.reload();
+}
 
+function chance() {
+  if(nbEssai ==0){
+    alert("tu as perdu")
+    reset();
+  }document.getElementById("essai").innerHTML= nbEssai;
+}
 
 // script
-shuffleArray(carte);
-for (var i = 0; i < choixJoueur.length; i++) {
-  document.getElementsByTagName("img")[i].src=carte[i];
-    choixJoueur[i].onclick = function () {
-      this.style.opacity= 1;
-      comparer.push(this);
-      var ordi=comparer.length == 2;
-      comparaison();
-    }
-
-}
